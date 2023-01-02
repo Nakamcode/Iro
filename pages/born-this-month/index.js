993f3484d2e99f6c7c8c71c13ba22fb187b996c3
Afriyie Samuel Kwesi <afriyiesamuel36@gmail.com>
2023-01-02 12:55:08 -0800

    feat: added categories and born-this-month pages

---
+++ pages/born-this-month/index.js
@@ -0,0 +1,98 @@
+import { characteristics, dummyCelebrities, heroCategories } from '#/dummy';
+import Image from 'next/image';
+import React from 'react'
+import CategoryCard from '~/Cards/CategoryCard';
+import Celebrity from '~/Cards/Celebrity';
+import Autoscroll from '~/core/Autoscroll';
+import Layout from '~/layout/Layout';
+
+const BornThisMonth = () => {
+  return (
+    <Layout>
+        <section className="px-24 py-20">
+            <header className="w-full flex flex-col items-center justify-center mb-10">
+                <div className="flex justify-center space-x-3 leading-tight my-4">
+                    <Image src="/aries.svg" alt="Aries symbol" width={35} height={35} />
+                    <h2 className="text-xl md:text-2xl lg:text-4xl text-center font-semibold font-display">
+                        Born This Month
+                    </h2>
+                </div>
+                <p className="text-xl font-medium w-3/4 text-center">Iro&apos;s born this month are passionate, motivated, and confident people who builds community with their cheerful disposition and relentless determination.</p>
+            </header>
+            <div className="py-20 grid grid-cols-2 items-center gap-10">
+                <div className="w-[80%]">
+                    <h2 className="text-5xl font-semibold mb-8">Characteristics</h2>
+                    <ul className="space-y-6">
+                        <li className="flex items-center">
+                            <div className="bg-[#E849EB] p-2 rounded-full mr-3">
+                                <Image src="/leo_w.svg" alt="leo logo" height={20} width={20}/>
+                            </div>
+                            <span className="font-medium text-xl">Aries are attracted to people who can keep up with them.</span>
+                        </li>
+                        <li className="flex items-center">
+                            <div className="bg-[#E849EB] p-2 rounded-full mr-3">
+                                <Image src="/leo_w.svg" alt="leo logo" height={20} width={20}/>
+                            </div>
+                            <span className="font-medium text-xl">Creative Skills of Aries natives are heart winning.</span>
+                        </li>
+                        <li className="flex items-center">
+                            <div className="bg-[#E849EB] p-2 rounded-full mr-3">
+                                <Image src="/leo_w.svg" alt="leo logo" height={20} width={20}/>
+                            </div>
+                            <span className="font-medium text-xl">They are very Optimistic about everything.</span>
+                        </li>
+                    </ul>
+                </div>
+                <div className="grid grid-cols-3 gap-6">
+                    {characteristics.map((data) => 
+                        <CategoryCard data={data} key={data.id}/>
+                    )}
+                </div>
+            </div>
+        </section>
+        <Autoscroll/>
+        <section
+            id="aries-celebrities"
+            className="px-2 md:px-12 lg:px-24 my-20"
+        >
+            <div className="grid grid-cols-3 gap-8">
+            {dummyCelebrities.map(celebrity => {
+                return <Celebrity celebrity={celebrity} key={celebrity.id} />;
+            })}
+            </div>
+        </section>
+        <Autoscroll/>
+        <section className="px-24 py-20">
+            <div className="grid grid-cols-3 gap-8">
+                {dummyCelebrities.map(celebrity => {
+                    return <Celebrity celebrity={celebrity} key={celebrity.id} />;
+                })}
+            </div>
+        </section>
+        <section
+            id=""
+            className="grid grid-cols-2 items-center mt-20 mb-40 px-24"
+            >
+              <div className="w-[70%] space-y-6">
+                <h1 className="text-3xl font-display font-bold">Recommendation</h1>
+                <p className="font-medium text-xl">The fact that people have achieved a great deal for themselves shows you that you can achieve the same thing.</p>
+                <p className="font-medium text-xl">There are a number of categories of signs that you can explore in order to get inspiration and motivation.</p>
+              </div>
+              <div className="w-full grid grid-cols-3 gap-10">
+                {heroCategories.map((categoryData) => 
+                  <CategoryCard data={categoryData} key={categoryData.id}/>
+                )}
+              </div>
+        </section>
+        <section className="px-24 py-20">
+            <div className="grid grid-cols-3 gap-8">
+                {dummyCelebrities.map(celebrity => {
+                    return <Celebrity celebrity={celebrity} key={celebrity.id} />;
+                })}
+            </div>
+        </section>
+    </Layout>
+  )
+}
+
+export default BornThisMonth
---
+++ pages/categories.js
@@ -0,0 +1,54 @@
+import { categories, dummyCelebrities } from "#/dummy";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import CategoryCard from "~/Cards/CategoryCard";
+import Celebrity from "~/Cards/Celebrity";
+import Autoscroll from "~/core/Autoscroll";
+import Button from "~/core/Button";
+import Layout from "~/layout/Layout";
+
+const Categories = () => {
+	return (
+		<Layout>
+			<section className="px-24 py-20">
+				<header className="w-full flex flex-col items-center justify-center mb-10">
+					<h1 className="text-5xl font-bold mb-4">Categories</h1>
+					<p className="text-xl font-medium">
+						Finding inspiration based on your zodiac sign is something I am are
+						passionate about.
+					</p>
+				</header>
+				<div className="grid grid-cols-6 gap-6">
+					{categories.map((data) => (
+						<Link href={`/categories/${data.name}`} key={data.id}>
+							<CategoryCard data={data} key={data.id} />
+						</Link>
+					))}
+				</div>
+			</section>
+			<Autoscroll />
+			<section id="aries-celebrities" className="px-2 md:px-12 lg:px-24 my-20">
+				<div className="flex justify-center space-x-3 leading-tight my-4">
+					<Image src="/aries.svg" alt="Aries symbol" width={35} height={35} />
+					<h2 className="text-xl md:text-2xl lg:text-4xl text-center font-semibold font-display">
+						Born Today
+					</h2>
+				</div>
+				<p className="text-center w-[75%] mx-auto mb-8 text-xl font-medium">
+					Aries is a passionate, motivated, and confident leader who builds
+					community with their cheerful disposition and relentless
+					determination.
+				</p>
+				<div className="grid grid-cols-3 gap-8">
+					{dummyCelebrities.map((celebrity) => {
+						return <Celebrity celebrity={celebrity} key={celebrity.id} />;
+					})}
+				</div>
+				<Button className="mt-8">More</Button>
+			</section>
+		</Layout>
+	);
+};
+
+export default Categories;
