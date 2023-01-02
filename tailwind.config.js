7bc0d0e6d3679b29f8f00248099bb2dfe713ccc0
Afriyie Samuel Kwesi <afriyiesamuel36@gmail.com>
2023-01-01 19:14:52 -0800

    feat: added fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
@@ -6,7 +7,39 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Inter", ...defaultTheme.fontFamily.sans],
+				display: [
+					'"Lato"',
+					"system-ui",
+					"-apple-system",
+					"BlinkMacSystemFont",
+					"Segoe UI",
+					"Roboto",
+					"Oxygen",
+					"Ubuntu",
+					"Cantarell",
+					"Open Sans",
+					"Helvetica Neue",
+					"sans-serif",
+				],
+				body: [
+					'"Inter"',
+					"system-ui",
+					"-apple-system",
+					"BlinkMacSystemFont",
+					"Segoe UI",
+					"Roboto",
+					"Oxygen",
+					"Ubuntu",
+					"Cantarell",
+					"Open Sans",
+					"Helvetica Neue",
+					"sans-serif",
+				],
+			},
+		},
 	},
 	plugins: [],
 };
