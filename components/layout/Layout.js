0b4919b4b6726c0cc02b3a14e116d358dcedb0d2
Afriyie Samuel Kwesi <afriyiesamuel36@gmail.com>
2023-01-08 11:20:16 -0800

    feat: fix website stretching too much on very large screens

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -4,7 +4,7 @@ import Header from "./Header";
 import { Widget } from "@typeform/embed-react";
 const Layout = ({ children }) => {
 	return (
-		<>
+		<div className="max-w-[2000px] mx-auto">
 			<Widget
 				id="rXv5FPQf"
 				style={{ width: "50%" }}
@@ -13,7 +13,7 @@ const Layout = ({ children }) => {
 			<Header />
 			<div className="relative top-[80px] lg:top-14">{children}</div>
 			<Footer />
-		</>
+		</div>
 	);
 };
 export default Layout;
